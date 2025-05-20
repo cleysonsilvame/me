903424eca9d53b4736a6bf962e7027ec1a4dea1d
Cleyson Silva <cleysonsilva.me@gmail.com>
2025-05-19 22:50:57 -0300

    feat: add new projects to the projects list with descriptions and tech stacks

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -10,6 +10,36 @@ export const projects = [
     techStack: ['ReactJS', 'Vite', 'Github API'],
   },
   {
+    title: 'Ignite Shop',
+    description:
+      'An e-commerce application developed. Features include product browsing, shopping cart functionality, and Stripe payment integration. Test the payment system using card number 4242 4242 4242 4242 with any future expiration date.',
+    image:
+      'https://raw.githubusercontent.com/cleysonsilvame/desafio-04-ignite-react-ignite-shop/refs/heads/main/.github/iframe.png',
+    link: 'https://github.com/cleysonsilvame/desafio-04-ignite-react-ignite-shop',
+    preview: 'https://ignite-shop.cleysonsilva.dev/',
+    techStack: ['React', 'Next.js', 'TypeScript', 'Stitches', 'Stripe API'],
+  },
+  {
+    title: 'Coffee Delivery',
+    description:
+      'Coffee Delivery is a fictional e-commerce application for selling coffee, where users can select coffees, add them to cart, fill in delivery address, and choose payment method. Project developed as a challenge for Rocketseat Ignite.',
+    image:
+      'https://raw.githubusercontent.com/cleysonsilvame/desafio-02-react-2023-coffee-delivery/refs/heads/main/.github/iframe.png',
+    link: 'https://github.com/cleysonsilvame/desafio-02-react-2023-coffee-delivery',
+    preview: 'https://coffee-delivery.cleysonsilva.dev/',
+    techStack: ['React', 'TypeScript', 'Vite', 'Styled Components'],
+  },
+  {
+    title: 'Moveit',
+    description:
+      'Moveit is a gamified application designed to help those of us who spend long hours in front of the computer and often forget to exercise.',
+    image:
+      'https://raw.githubusercontent.com/cleysonsilvame/moveit/refs/heads/main/.github/iframe.png',
+    link: 'https://github.com/cleysonsilvame/moveit',
+    preview: 'https://moveit.cleysonsilva.dev/',
+    techStack: ['ReactJS', 'Typescript'],
+  },
+  {
     title: 'Ignews',
     description:
       'A blog platform with Stripe payment integration for subscriptions. You can test the payment system using the test card number 4242 4242 4242 4242 with any future expiration date.',
